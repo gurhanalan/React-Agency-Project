d5a6260a04cffdf0374def4742b0301fe5649960
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-10-04 15:01:29 +0300

    Git init, add files

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Intro from "./components/intro/Intro";
+import NavBar from "./components/navbar/NavBar";
+
+const App = () => {
+    return (
+        <div>
+            <Intro />
+        </div>
+    );
+};
+
+export default App;
